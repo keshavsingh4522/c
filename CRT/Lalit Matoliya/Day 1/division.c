2c75ae5627a4bd5d55dbed793e1a18bba3f58d8b
Keshav Singh <keshavsingh4522@gmail.com>
2020-08-05 21:12:43 +0530

    modular division number_system

---
+++ CRT/Lalit Matoliya/Day 1/division.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+  int x=4,y=3;
+  printf("%d\n", x/y);
+  printf("%d\n", -x/y);
+  printf("%d\n", x/-y);
+  printf("%d\n", -x/-y);
+}
---
+++ CRT/Lalit Matoliya/Day 1/number_system.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+  int x=32,y=032,z=0x32;
+  /*x is decimal number
+    y is octal number
+    z is hexa decimal number
+  */
+  printf("x=%d\ny=%d\nz=%d\n", x,y,z);
+}
