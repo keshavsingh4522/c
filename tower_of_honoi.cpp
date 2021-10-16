41c7659fb571ccdaf1982897921ed035a41503af
Prithu-Nagar <56398437+Prithu-Nagar@users.noreply.github.com>
2021-10-16 17:15:19 +0530

    Update tower_of_honoi.cpp
    imported new header files

--- tower_of_honoi.cpp
+++ tower_of_honoi.cpp
@@ -1,5 +1,6 @@
-#include"conio.h"
-#include"stdio.h"
+#include<iostream.h>
+#include<bits/stdc++.h>
+using namespace std;
 void tower(int,char,char,char);
 void main()
 {
@@ -14,8 +15,10 @@ getch();
 void tower(int n,char fpeg,char auxpeg,char topeg)
 {
     if(n==1)
-     {printf("move disk 1 from %c peg to %c peg \n",fpeg,auxpeg);
-     return;}
+     {
+         printf("move disk 1 from %c peg to %c peg \n",fpeg,auxpeg);
+         return;
+    }
     tower(n-1,fpeg,topeg,auxpeg);
     printf("move disk %d frmo %c peg to %c peg\n",n,fpeg,auxpeg);
     tower(n-1,auxpeg,fpeg,topeg);
