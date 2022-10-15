28d220fc041c18667039575962731725744a1ffa
Atharva Saraiya <98383314+Atharvasaraiya@users.noreply.github.com>
2022-10-15 23:34:53 +0530

    Update Fibonacci Series up to n number.cpp

--- Hactoberfest2020/Fibonacci Series up to n number.cpp
+++ Hactoberfest2020/Fibonacci Series up to n number.cpp
@@ -3,31 +3,31 @@ using namespace std;
 
 int main()
 {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
+    int n, k1 = 0, k2 = 1, nterm = 0;
 
     cout << "Enter the number of terms: ";
     cin >> n;
 
-    cout << "Fibonacci Series: ";
+    cout << "Check Fibonacci Series: ";
 
     for (int i = 1; i <= n; ++i)
     {
         // Prints the first two terms.
         if(i == 1)
         {
-            cout << " " << t1;
+            cout << " " << k1;
             continue;
         }
         if(i == 2)
         {
-            cout << t2 << " ";
+            cout << k2 << " ";
             continue;
         }
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
+        nterm = k1 + k2;
+        k1 = k2;
+        k2 = nterm;
         
-        cout << nextTerm << " ";
+        cout << nterm << " ";
     }
     return 0;
 }
