c831d571510cb4085eb58b7578e0a8423cbf5888
Rahul Vijan <69571769+rv2442@users.noreply.github.com>
2022-10-29 01:54:39 +0530

    Adding descriptive comments
    These comments will help new people, better understand the code

--- Hactoberfest2020/Fibonacci Series up to n number.cpp
+++ Hactoberfest2020/Fibonacci Series up to n number.cpp
@@ -1,10 +1,13 @@
+/* Global includes */
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    /* Initialising */
     int n, t1 = 0, t2 = 1, nextTerm = 0;
 
+    /* Prints following string on screen and then takes input */
     cout << "Enter the number of terms: ";
     cin >> n;
 
@@ -12,7 +15,7 @@ int main()
 
     for (int i = 1; i <= n; ++i)
     {
-        // Prints the first two terms.
+        /* Prints the first two terms (Exceptional). */
         if(i == 1)
         {
             cout << " " << t1;
@@ -23,6 +26,10 @@ int main()
             cout << t2 << " ";
             continue;
         }
+        
+        /*  Adds nth term to (n-1)th term and assignes it's sum to (n+1)th term
+         *  t_(n+1) = [t_n + t_(n-1)] 
+         */
         nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
